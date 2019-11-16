a5618fc069eda566755b41fda16042c288b11fc1
rhivent <zzzzjrjr@gmail.com>
2019-11-16 19:43:10 +0700

    ready to consume token from provider

--- consumer/routes/web.php
+++ consumer/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +16,42 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/redirect', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+
+    $query = http_build_query([
+        'client_id' => '4',
+        'redirect_uri' => 'http://localhost:9000/callback',
+        'response_type' => 'code',
+        'scope' => '',
+        'state' => $state,
+    ]);
+
+    return redirect('http://localhost:8000/oauth/authorize?'.$query);
+})->name('gettoken');
+
+
+Route::get('/callback', function (Request $request) {
+    $state = $request->session()->pull('state');
+
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class
+    );
+
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://localhost:8000/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => '4',
+            'client_secret' => 'CZwTcZAK1LNTw119nnxxGeZWnICRK3aquLHF3mvP',
+            'redirect_uri' => 'http://localhost:9000/callback',
+            'code' => $request->code,
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
 });
